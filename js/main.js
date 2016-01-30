1ef4298038fb8bd4db8dccdf1f7f8cb9cc272557
Jerry Ngov <jcngov@yahoo.com>
2016-01-30 11:30:53 -0800

    make data model and startGame()

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 console.log("START");
+
+// Data Model:
+
+var board = ["", "", "", "",
+             "", "", "", "",
+             "", "", "", "",
+             "", "", "", "",
+             "", "", "", ""
+            ];
+
+var flippedBoard = ["1", "2", "3", "4",
+                    "5", "6", "7", "8",
+                    "9", "10", "1", "2",
+                    "3", "4", "5", "6",
+                    "7", "8", "9", "10"
+                   ];
+
+var time;
+var player1 = "Player 1";
+var player2 = "Player 2";
+var win = false;
+
+// 1. Start Game:
+
+function startGame() {
+  win = false;
+  player1 = "Player 1";
+  var board = ["", "", "", "",
+             "", "", "", "",
+             "", "", "", "",
+             "", "", "", "",
+             "", "", "", ""
+            ];
+}
+
+
