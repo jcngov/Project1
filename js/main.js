b3d3a53c71663902b992983d406f8c324f8318a2
Jerry Ngov <jcngov@yahoo.com>
2016-01-30 18:05:40 -0800

    add pickCells() and complete()

--- js/main.js
+++ js/main.js
@@ -1,52 +1,70 @@
 console.log("START");
 
 // Data Model:
-var back = [];
+
 var tiles = [0, 1, 2, 3,
             4, 5, 6, 7,
             8, 9, 0, 1,
             2, 3, 4, 5,
             6, 7, 8, 9
             ];
-var flipped = 0;
+var matches = 0;
 var time;
-var player1 = "Player 1";
-var player2 = "Player 2";
+var player;
 var win = false;
 
 // 1. Start Game:
 
-function shuffleArray(tiles) {
-  var tiles = [0, 1, 2, 3,
-               4, 5, 6, 7,
-               8, 9, 0, 1,
-               2, 3, 4, 5,
-               6, 7, 8, 9
-              ];
-    for (var i = tiles.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = tiles[i];
-        tiles[i] = tiles[j];
-        tiles[j] = temp;
-    }
-    return tiles;
-}
+// function shuffleArray(tiles) {
+//   var tiles = [0, 1, 2, 3,
+//                4, 5, 6, 7,
+//                8, 9, 0, 1,
+//                2, 3, 4, 5,
+//                6, 7, 8, 9
+//               ];
+//     for (var i = tiles.length - 1; i > 0; i--) {
+//         var j = Math.floor(Math.random() * (i + 1));
+//         var temp = tiles[i];
+//         tiles[i] = tiles[j];
+//         tiles[j] = temp;
+//     }
+//     return tiles;
+// }
 
 function startGame() {
   win = false;
   player1 = "Player 1";
-  var back = [];
-  var flipped = 0;
-  return shuffleArray(tiles);
+  var matches = 0;
+  // return shuffleArray(tiles);
 }
 
-
-function flipTiles(cell) {
-  for (var i = 0; i < tiles.length; i += 1) {
-    back.push(tiles[cell]);
+function pickCells(cellOne, cellTwo) {
+  var pick1 = tiles[cellOne];
+  var pick2 = tiles[cellTwo];
+  if (pick1 === pick2) {
+    matches += 1;
+    console.log('cool');
+    complete();
+  }
+  else {
+    console.log('try again');
   }
 }
 
+function complete() {
+  if (matches === 10) {
+    console.log('completed');
+  }
+}
+// function firstFlip(cellOn) {
 
+// }
 
+// $('#cell').on("click", function(event) {
+//   $('#cell').css({"background-color": "red"});
+// });
+
+// $('#cell1').on("click", function(event) {
+//   $('#cell1').css({"background-color": "red"});
+// });
 
