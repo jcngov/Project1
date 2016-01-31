ccdb4f71de5dc5b2f30107bfa18942ee475c5917
Jerry Ngov <jcngov@yahoo.com>
2016-01-30 18:16:31 -0800

    make memory grid

--- js/main.js
+++ js/main.js
@@ -15,40 +15,41 @@ var win = false;
 
 // 1. Start Game:
 
-// function shuffleArray(tiles) {
-//   var tiles = [0, 1, 2, 3,
-//                4, 5, 6, 7,
-//                8, 9, 0, 1,
-//                2, 3, 4, 5,
-//                6, 7, 8, 9
-//               ];
-//     for (var i = tiles.length - 1; i > 0; i--) {
-//         var j = Math.floor(Math.random() * (i + 1));
-//         var temp = tiles[i];
-//         tiles[i] = tiles[j];
-//         tiles[j] = temp;
-//     }
-//     return tiles;
-// }
+function shuffleArray(tiles) {
+  var tiles = [0, 1, 2, 3,
+               4, 5, 6, 7,
+               8, 9, 0, 1,
+               2, 3, 4, 5,
+               6, 7, 8, 9
+              ];
+    for (var i = tiles.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = tiles[i];
+        tiles[i] = tiles[j];
+        tiles[j] = temp;
+    }
+    return tiles;
+}
 
 function startGame() {
   win = false;
   player1 = "Player 1";
   var matches = 0;
-  // return shuffleArray(tiles);
+  return shuffleArray(tiles);
 }
 
 function pickCells(cellOne, cellTwo) {
+  var newBoard = shuffleArray(tiles);
   var pick1 = tiles[cellOne];
   var pick2 = tiles[cellTwo];
-  if (pick1 === pick2) {
-    matches += 1;
-    console.log('cool');
-    complete();
-  }
+   if (pick1 === pick2) {
+     matches += 1;
+     console.log('cool');
+     complete();
+   }
   else {
-    console.log('try again');
-  }
+     console.log('try again');
+   }
 }
 
 function complete() {
@@ -56,9 +57,7 @@ function complete() {
     console.log('completed');
   }
 }
-// function firstFlip(cellOn) {
 
-// }
 
 // $('#cell').on("click", function(event) {
 //   $('#cell').css({"background-color": "red"});
